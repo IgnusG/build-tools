becedbab46e267937b395b7929be19a6c5be5460
IgnusG <6438760+IgnusG@users.noreply.github.com>
2022-11-16 22:23:42 +0100

    Remove .tools in favor of a python enabled project for auto-updates

--- README.md
+++ README.md
@@ -14,13 +14,9 @@ Thanks for your interest! Just a few more steps to follow and we can get your lo
 
 ### Setting up your repository clone
 
-1. Install node (see package.json's `engines.node` for the version), yarn (no need for any specific version) and python (any version above/at 3)
+1. Install node (see package.json's `engines.node` for the version), yarn (no need for any specific version), python (any version above/at 3) and poetry (see https://python-poetry.org/docs/#installing-with-the-official-installer)
 2. Run `yarn install` to install all required dependencies
 3. Run `yarn prepare-local` to prepare editor integrations (VS Code) & commit hooks
-
-> This repo already comes with all dependencies and tools using a PnP (Plug-and-Play) zero install architecture
->
-> TypeScript dependencies are kept in .yarn/cache and tool executables are inside of .tools (bundled as pre-packaged apps)
 
 ### Release
 
--- prepare.ts
+++ prepare.ts
@@ -1,7 +1,8 @@
 #!/usr/bin/env -S yarn pnpify ts-node -T --esm
 
 import { $, chalk, echo, fs, which } from "zx";
-import { spinner } from "zx/experimental";
+
+const ci = process.argv.includes("--ci");
 
 const VSCODE_PATH = "./.vscode";
 const VSCODE_SETTINGS_TEMPLATE = `${VSCODE_PATH}/settings-template.json`;
@@ -9,37 +10,43 @@ const VSCODE_SETTINGS = `${VSCODE_PATH}/settings.json`;
 
 echo(`prepare.ts: Preparing workspace`);
 
-if (!(await fs.stat(VSCODE_SETTINGS))) {
-	echo(
-		`prepare.ts: ${chalk.blue("Copying VS Code template settings")} into ${VSCODE_SETTINGS}... `,
-	);
+async function prepareVSCode() {
+	if (ci) return;
 
-	await fs.copyFile(VSCODE_SETTINGS_TEMPLATE, VSCODE_SETTINGS);
-} else {
-	echo(
-		`prepare.ts: ${chalk.blue(
-			"VS Code workspace settings found",
-		)} - skipped overwriting them (see ${VSCODE_SETTINGS_TEMPLATE})`,
-	);
+	if (!(await fs.stat(VSCODE_SETTINGS))) {
+		echo(
+			`prepare.ts: ${chalk.blue("Copying VS Code template settings")} into ${VSCODE_SETTINGS}... `,
+		);
+
+		await fs.copyFile(VSCODE_SETTINGS_TEMPLATE, VSCODE_SETTINGS);
+	} else {
+		echo(
+			`prepare.ts: ${chalk.blue(
+				"VS Code workspace settings found",
+			)} - skipped overwriting them (see ${VSCODE_SETTINGS_TEMPLATE})`,
+		);
+	}
 }
 
-try {
-	await which("python");
-} catch {
-	echo(
-		`prepare.ts: ${chalk.red(
-			"Failed",
-		)} - you must install a python executable as we use it to run pre-commit hooks`,
-	);
+async function prepareCommitHooks() {
+	try {
+		await which("poetry");
+	} catch {
+		echo(
+			`prepare.ts: ${chalk.red(
+				"Failed",
+			)} - you must install poetry - see https://python-poetry.org/docs/#installation for a guide`,
+		);
 
-	process.exit(1);
+		process.exit(1);
+	}
+
+	echo(`prepare.ts: Installing commit hooks`);
+
+	await $`poetry install --no-interaction`;
+
+	if (!ci) await $`poetry run pre-commit install -t commit-msg -t pre-commit`;
 }
 
-echo(`prepare.ts: ${chalk.blue("Installing git hooks")} using pre-commit...`);
-
-await spinner(async () => {
-	await $`python ./.tools/pre-commit.pyz install`;
-	await $`python ./.tools/pre-commit.pyz install --hook-type commit-msg`;
-});
-
-echo(`prepare.ts: ${chalk.green("All done! Workspace prepared successfully")}`);
+await prepareVSCode();
+await prepareCommitHooks();
